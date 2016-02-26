27582af0c4099cf86bc74ce1fcded6519c005370
olivier.pitton <olivier.pitton@kubity.com>
2016-02-26 10:34:33 +0100

    Refactor endpoints + add lib

--- src/main/com/traceyourfriend/inventory/Inventory.java
+++ src/main/com/traceyourfriend/inventory/Inventory.java
@@ -1,11 +1,14 @@
 package main.com.traceyourfriend.inventory;
 
+import com.google.gson.Gson;
+import main.com.traceyourfriend.beans.User;
 import main.com.traceyourfriend.dao.UsersDAO;
-import org.codehaus.jettison.json.JSONArray;
+import org.apache.commons.lang3.StringUtils;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
+import java.sql.SQLException;
+import java.util.List;
 
 
 /**
@@ -15,8 +18,11 @@ import javax.ws.rs.core.Response;
  */
 
 @Path("/users")
+@Produces(MediaType.APPLICATION_JSON)
 public class Inventory {
 
+
+	private final UsersDAO dao = new UsersDAO();
 
     /**
      * This method will return all users that are listed
@@ -29,25 +35,9 @@ public class Inventory {
      * @throws Exception
      */
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response findAll() throws Exception {
-
-        String returnString;
-        Response rb = null;
-        JSONArray json;
-        try {
-            UsersDAO dao = new UsersDAO();
-
-            json = dao.findAll(); //Erreur ne recupere pas les ligne dans la base de données
-            returnString = json.toString();
-
-            rb =  Response.ok(returnString).build();
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return rb;
+    public String findAll() throws SQLException {
+		List<User> users = dao.findAll();
+		return new Gson().toJson(users);
     }
 
     /**
@@ -64,68 +54,14 @@ public class Inventory {
      */
     @Path("/{email}")
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response findWithEmail(
-            @PathParam("email") String email)
-            throws Exception {
-
-        String returnString;
-
-        JSONArray json;
-
-        try {
-
-            UsersDAO dao = new UsersDAO();
-
-            json = dao.findWithEmail(email);
-            returnString = json.toString();
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-            return Response.status(500).entity("Server was not able to process your request").build();
-        }
-
-        return Response.ok(returnString).build();
+    public String findWithEmail(@PathParam("email") String email) throws SQLException {
+		if (StringUtils.isEmpty(email)) {
+			throw new NotFoundException();
+		}
+		User user = dao.findWithEmail(email);
+		if (user == null) {
+			throw new NotFoundException();
+		}
+		return new Gson().toJson(user);
     }
-
-
-
-    /**
-     * This method will return the specific name of user is looking for.
-     * It is very similar to the method findAll except this method uses the
-     * PathParam to bring in the data.
-     *
-     * Example would be:
-     * http://localhost:8080/TraceYourFriends/api/users/search/aniss
-     *
-     * @param name - product email name
-     * @return - json array results list from the database
-     * @throws Exception
-     */
-    @Path("search/{name}")
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response findWithName(
-            @PathParam("name") String name)
-            throws Exception {
-
-        String returnString;
-
-        JSONArray json;
-
-        try {
-
-            UsersDAO dao = new UsersDAO();
-
-            json = dao.findWithName(name);
-            returnString = json.toString();
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-            return Response.status(500).entity("Server was not able to process your request").build();
-        }
-
-        return Response.ok(returnString).build();
-    }
-
 }
