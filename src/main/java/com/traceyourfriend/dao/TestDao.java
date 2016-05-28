bae606e4eede8906eeaab1a9c0b546f6f7dc4e0a
Kaci Kelfaoui <Kaci Kelfaoui>
2016-05-28 14:28:40 +0200

    Fix Load Friends

---
+++ src/main/java/com/traceyourfriend/dao/TestDao.java
@@ -0,0 +1,31 @@
+package com.traceyourfriend.dao;
+
+import com.traceyourfriend.beans.User;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import java.time.Clock;
+
+/**
+ * Created by pyrob on 28/05/2016.
+ */
+public class TestDao {
+
+    DAO<User> userDao = new UsersDAO();
+
+    public static void main (String[] args){
+
+        for(int i = 1; i < 5; i++){
+
+
+
+
+
+        }
+
+
+        System.out.println("toto");
+    }
+}
