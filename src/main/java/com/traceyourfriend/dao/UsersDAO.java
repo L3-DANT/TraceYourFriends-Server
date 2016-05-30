7eb0bdb3a3a675f54e73e5af6f4ff9ddc85f46cf
Kaci Kelfaoui <Kaci Kelfaoui>
2016-05-30 10:30:50 +0200

    Add LoadRequests

--- src/main/java/com/traceyourfriend/dao/UsersDAO.java
+++ src/main/java/com/traceyourfriend/dao/UsersDAO.java
@@ -24,7 +24,8 @@ public class UsersDAO implements DAO{
     private static final String SQL_SELECT_USER_BY_NAME = "SELECT * FROM users WHERE name = ?";
 	private static final String SQL_INSERT_USER = "INSERT INTO users (name, email, password) VALUES (?, ?, ?)";
     private static final String SQL_SELECT_USER = "SELECT * FROM users";
-	private static final String SQL_SELECT_FRIENDS = "SELECT DISTINCT u.name FROM users u, amis a WHERE ID_USER1=? AND a.ID_USER2=u.id";
+	private static final String SQL_SELECT_FRIENDS = "SELECT DISTINCT u.name FROM users u, amis a WHERE a.ID_USER1=? AND a.ID_USER2=u.id";
+	private static final String SQL_SELECT_REQUESTS = "SELECT DISTINCT u.name FROM users u, demandes d WHERE d.ID_USER1=? AND d.ID_USER2=u.id";
 
     private final Connection connection = SQLConnection.getSQLCon().getDbCon();
 
@@ -148,5 +149,19 @@ public class UsersDAO implements DAO{
 				}
 			}
 		return user.getAmis();
+	}
+
+	public List<String> loadRequests(User user) throws SQLException {
+		try (PreparedStatement preparedStatement = connection.prepareStatement(SQL_SELECT_REQUESTS)) {
+			preparedStatement.setLong(1, user.getId());
+			try(ResultSet resultSet = preparedStatement.executeQuery()) {
+				while (resultSet.next()) {
+					user.addDemandeAmi(resultSet.getString(1));
+				}
+				preparedStatement.close();
+				resultSet.close();
+			}
+		}
+		return user.getDemandesAmi();
 	}
 }
--- src/main/java/com/traceyourfriend/utils/HashUser.java
+++ src/main/java/com/traceyourfriend/utils/HashUser.java
@@ -41,6 +41,7 @@ public class HashUser {
             usersMail.put(emailOrName, user);
             usersName.put(emailOrName, user);
             dao.loadFriends(user);
+            dao.loadRequests(user);
         }
         return user;
     }
